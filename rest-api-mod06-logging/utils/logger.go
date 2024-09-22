8ab18a2adb0782b76203ddbb62e52d4dac294a54
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2024-09-22 17:44:36 +0700

    feat: sample logging

--- rest-api-mod06-logging/utils/logger.go
+++ rest-api-mod06-logging/utils/logger.go
@@ -8,8 +8,33 @@ var Log = logrus.New()
 // Fields untuk memudahkan menambahkan fields ke dalam log
 type Fields = logrus.Fields
 
+// Custom log level for "Alert"
+const LogAlert logrus.Level = logrus.PanicLevel + 1
+
 func init() {
+	// Tambahkan formatter untuk log
 	Log.SetFormatter(&logrus.TextFormatter{
 		FullTimestamp: true,
 	})
+
+	// Register custom level "Alert"
+	Log.SetLevel(logrus.DebugLevel)
+	logrus.RegisterExitHandler(func() {
+		logrus.AddHook(newAlertHook())
+	})
+}
+
+type alertHook struct{}
+
+func newAlertHook() *alertHook {
+	return &alertHook{}
+}
+
+func (hook *alertHook) Levels() []logrus.Level {
+	return []logrus.Level{LogAlert}
+}
+
+func (hook *alertHook) Fire(entry *logrus.Entry) error {
+	entry.Message = "ALERT: " + entry.Message
+	return nil
 }
